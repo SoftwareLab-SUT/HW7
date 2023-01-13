9a0dbcb009ec09afce50766b2f3924ebf0f4bf04
Mira <najafi.mehraneh@gmail.com>
2023-01-13 21:52:43 +0330

    Another State or Strategy

--- MiniJava/src/main/java/codeGenerator/Address.java
+++ MiniJava/src/main/java/codeGenerator/Address.java
@@ -5,24 +5,43 @@ package codeGenerator;
  */
 
 public class Address {
+    public abstract class AddressType {
+        abstract TypeAddress getAddressType();
+    }
+
+    public class Direct extends AddressType {
+        @Override
+        TypeAddress getAddressType() { return TypeAddress.Direct; }
+    }
+
+    public class Indirect extends AddressType {
+        @Override
+        TypeAddress getAddressType() { return TypeAddress.Indirect; }
+    }
+
+    public class Imidiate extends AddressType {
+        @Override
+        TypeAddress getAddressType() { return TypeAddress.Imidiate; }
+    }
+
     public int num;
-    public TypeAddress Type;
+    public AddressType Type;
     public varType varType;
 
     public Address(int num, varType varType, TypeAddress Type) {
         this.num = num;
-        this.Type = Type;
+        setType(Type);
         this.varType = varType;
     }
 
     public Address(int num, varType varType) {
         this.num = num;
-        this.Type = TypeAddress.Direct;
+        setType(TypeAddress.Direct);
         this.varType = varType;
     }
 
     public String toString() {
-        switch (Type) {
+        switch (getType()) {
             case Direct:
                 return num + "";
             case Indirect:
@@ -31,5 +50,25 @@ public class Address {
                 return "#" + num;
         }
         return num + "";
+    }
+
+    public TypeAddress getType() {
+        return Type.getAddressType();
+    }
+
+    public void setType(TypeAddress typeAddress) {
+        switch (typeAddress) {
+            case Direct:
+                Type = new Address.Direct();
+                break;
+            case Indirect:
+                Type = new Address.Indirect();
+                break;
+            case Imidiate:
+                Type = new Address.Imidiate();
+                break;
+            default:
+                throw new IllegalArgumentException("address type non-existent");
+        }
     }
 }
